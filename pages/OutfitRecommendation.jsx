b36eb69239e84db999609352d5d5b6b0aaa3f381
ddelan <dtnguyen0216@gmail.com>
2023-12-04 13:41:33 -0600

    fixed reccomendation algorithm

--- pages/OutfitRecommendation.jsx
+++ pages/OutfitRecommendation.jsx
@@ -1,56 +1,61 @@
-import React, { useContext, useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert} from 'react-native';
-import { Picker } from '@react-native-picker/picker';
+import React, { useContext, useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from 'react-native';
 
 import WeatherDataContext from '../context/WeatherDataContext';
 import WeatherAPITools from '../weatherTools';
 
-
 const defaultClothingItems = {
-  footwear: [
-    { name: 'Sneakers', temperature: { min: 30, max: Infinity } },
-    { name: 'Sandals', temperature: { min: 60, max: Infinity } },
-    { name: 'Boots', temperature: { min: -Infinity, max: 30 } },
+  Tops: [
+    { name: 'T-shirt', temperature: { min: 60, max: Infinity } },
+    { name: 'Sweatshirt', temperature: { min: 35, max: 60 } },
+    { name: 'Tanktop', temperature: { min: 60, max: Infinity } },
+    { name: 'Puffer Jacket', temperature: { min: -Infinity, max: 35 } },
+    { name: 'Parka', temperature: { min: -Infinity, max: 35 } },
   ],
-  legs: [
-    { name: 'Jeans', temperature: { min: 30, max: Infinity } },
+  Bottoms: [
+    { name: 'Jeans', temperature: { min: -Infinity, max: Infinity } },
     { name: 'Shorts', temperature: { min: 60, max: Infinity } },
     { name: 'Sweatpants', temperature: { min: 30, max: Infinity } },
+    { name: 'Cargo Pants', temperature: { min: 30, max: Infinity } },
   ],
-  upperBody: [
-    { name: 'T-shirt', temperature: { min: 60, max: Infinity } },
-    { name: 'Sweatshirt', temperature: { min: 30, max: 60 } },
-    { name: 'Tanktop', temperature: { min: 60, max: Infinity } },
-    { name: 'Winter Jacket', temperature: { min: -Infinity, max: 30 } },
+  Footwear: [
+    { name: 'Sneakers', temperature: { min: 30, max: Infinity } },
+    { name: 'Sandals', temperature: { min: 60, max: Infinity } },
+    { name: 'Winter Boots', temperature: { min: -Infinity, max: 35 } },
+    { name: 'Timbs', temperature: { min: 30, max: Infinity } },
   ],
 };
 
 const OutfitRecommendation = () => {
-  const [selectedCategory, setSelectedCategory] = useState('footwear');
   const [recommendedItems, setRecommendedItems] = useState({});
-
+  const [currentTemperature, setCurrentTemperature] = useState('');
+  const [cityAndState, setCityAndState] = useState('');
   const weatherData = useContext(WeatherDataContext);
-  const tools = new WeatherAPITools(weatherData);
+
+  const fetchWeatherDetailsAndRecommendOutfit = async () => {
+    try {
+      const tools = new WeatherAPITools(weatherData);
+      const [temp] = tools.getCurrentWeather();
+      setCurrentTemperature(temp);
+      recommendOutfitBasedOnTemperature(parseFloat(temp));
+
+      const [city, state] = tools.getLocationDetails();
+      setCityAndState(`${city}, ${state}`);
+    } catch (error) {
+      Alert.alert('Error', `Failed to fetch weather data: ${error.message || error}`);
+    }
+  };
 
   useEffect(() => {
-    const fetchWeatherAndRecommendOutfit = async () => {
-      try {
-        const currentTemp = await tools.getCurrentTemperature(); // Assuming this method exists and returns the current temperature
-        recommendOutfitBasedOnTemperature(currentTemp);
-      } catch (error) {
-        Alert.alert('Error', 'Failed to fetch weather data.');
-      }
-    };
-
-    fetchWeatherAndRecommendOutfit();
-  }, []);
+    fetchWeatherDetailsAndRecommendOutfit();
+  }, [weatherData]);
 
   const recommendOutfitBasedOnTemperature = (temperature) => {
     let newRecommendedItems = {};
 
     Object.keys(defaultClothingItems).forEach(category => {
-      const categoryItems = defaultClothingItems[category];
-      const matchedItem = categoryItems.find(
+      const shuffledItems = [...defaultClothingItems[category]].sort(() => 0.5 - Math.random());
+      const matchedItem = shuffledItems.find(
         (item) => temperature >= item.temperature.min && temperature <= item.temperature.max
       );
 
@@ -62,35 +67,27 @@ const OutfitRecommendation = () => {
     setRecommendedItems(newRecommendedItems);
   };
 
-  const handleNewOutfit = () => {
-    setRecommendedItems({});
-  };
-
   return (
     <View style={styles.container}>
-      <Picker
-        selectedValue={selectedCategory}
-        style={styles.picker}
-        onValueChange={(itemValue) => setSelectedCategory(itemValue)}
-      >
-        <Picker.Item label="Footwear" value="footwear" />
-        <Picker.Item label="Legs" value="legs" />
-        <Picker.Item label="Upper Body" value="upperBody" />
-      </Picker>
-
+      <Text style={styles.locationText}>
+        Location: {cityAndState}
+      </Text>
+      <Text style={styles.temperatureText}>
+        Current Temperature: {currentTemperature}°F
+      </Text>
       {Object.keys(recommendedItems).length > 0 && (
         <View style={styles.recommendationContainer}>
           <Text style={styles.recommendationText}>Outfit Recommendation:</Text>
           {Object.keys(recommendedItems).map((category) => (
             <Text key={category}>
-              {category}: {recommendedItems[category].name}
+              {category.charAt(0).toUpperCase() + category.slice(1)}: {recommendedItems[category].name}
             </Text>
           ))}
-          <TouchableOpacity style={styles.button} onPress={handleNewOutfit}>
-            <Text style={styles.buttonText}>Request New Outfit</Text>
-          </TouchableOpacity>
         </View>
       )}
+      <TouchableOpacity style={styles.button} onPress={fetchWeatherDetailsAndRecommendOutfit}>
+        <Text style={styles.buttonText}>Generate New Outfit</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -100,12 +97,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  input: {
-    borderWidth: 1,
-    padding: 10,
-    margin: 10,
-    width: 200,
   },
   button: {
     backgroundColor: 'blue',
@@ -122,12 +113,17 @@ const styles = StyleSheet.create({
   },
   recommendationText: {
     fontWeight: 'bold',
+    fontSize: 20,
     marginBottom: 10,
   },
-  picker: {
-    height: 50,
-    width: 150,
-    borderWidth: 1,
+  temperatureText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    margin: 10,
+  },
+  locationText: {
+    fontSize: 24,
+    fontWeight: 'bold',
     margin: 10,
   },
 });
